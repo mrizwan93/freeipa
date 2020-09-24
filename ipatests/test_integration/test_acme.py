47e0e769069cf12d8cdcc355eb09d10f873f8fb4
Mohammad Rizwan <myusuf@redhat.com>
2020-09-24 19:18:36 +0530

    External ca scenarios for ACME
    Moved the test to independent method and call them
    once for self-signed CA and second time for external CA
    
    Signed-off-by: Mohammad Rizwan <myusuf@redhat.com>

--- ipatests/test_integration/test_acme.py
+++ ipatests/test_integration/test_acme.py
@@ -12,7 +12,16 @@ from ipalib.constants import IPA_CA_RECORD
 from ipatests.test_integration.base import IntegrationTest
 from ipatests.pytest_ipa.integration import tasks
 from ipaplatform.osinfo import osinfo
+from ipaplatform.paths import paths
+from ipatests.test_integration.test_external_ca import (
+    install_server_external_ca_step1,
+    install_server_external_ca_step2,
+    ISSUER_CN,
+)
 
+
+IPA_CA = "ipa_ca.crt"
+ROOT_CA = "root_ca.crt"
 
 # RHEL does not have certbot.  EPEL's version is broken with
 # python-cryptography-2.3; likewise recent PyPI releases.
@@ -24,6 +33,156 @@ skip_certbot_tests = osinfo.id not in ['fedora',]
 skip_mod_md_tests = osinfo.id not in ['rhel','fedora',]
 
 CERTBOT_DNS_IPA_SCRIPT = '/usr/libexec/ipa/acme/certbot-dns-ipa'
+
+def acme_service_not_yet_enabled(mh):
+    # --fail makes curl exit code 22 when response status >= 400.
+    # ACME service should return 503 because it was not enabled yet.
+    mh.clients[0].run_command(
+        ['curl', '--fail', mh.acme_server],
+        ok_returncode=22,
+    )
+
+def enable_acme_service(mh):
+        mh.master.run_command(['ipa-acme-manage', 'enable'])
+
+        # wait a short time for Dogtag ACME service to observe config
+        # change and reconfigure itself to service requests
+        exc = None
+        for _i in range(5):
+            time.sleep(2)
+            try:
+                mh.clients[0].run_command(
+                    ['curl', '--fail', mh.acme_server])
+                break
+            except Exception as e:
+                exc = e
+        else:
+            raise exc
+
+def certbot_register(mh):
+        # service is enabled; registration should succeed
+        mh.clients[0].run_command(
+            [
+                'certbot',
+                '--server', mh.acme_server,
+                'register',
+                '-m', 'nobody@example.test',
+                '--agree-tos',
+                '--no-eff-email',
+            ],
+        )
+
+def certbot_certonly_standalone(mh):
+        # Get a cert from ACME service using HTTP challenge and Certbot's
+        # standalone HTTP server mode
+        mh.clients[0].run_command(
+            [
+                'certbot',
+                '--server', mh.acme_server,
+                'certonly',
+                '--domain', mh.clients[0].hostname,
+                '--standalone',
+            ],
+        )
+
+def certbot_revoke(mh):
+        # Assume previous certonly operation succeeded.
+        # Read certificate to learn serial number.
+        cert_path = \
+            f'/etc/letsencrypt/live/{mh.clients[0].hostname}/cert.pem'
+        data = mh.clients[0].get_file_contents(cert_path)
+        cert = x509.load_pem_x509_certificate(data, backend=default_backend())
+
+        # revoke cert via ACME
+        mh.clients[0].run_command(
+            [
+                'certbot',
+                '--server', mh.acme_server,
+                'revoke',
+                '--cert-name', mh.clients[0].hostname,
+                '--delete-after-revoke',
+            ],
+        )
+
+        # check cert is revoked (kinit already performed)
+        result = mh.master.run_command(
+            ['ipa', 'cert-show', str(cert.serial_number), '--raw']
+        )
+        assert 'revocation_reason:' in result.stdout_text
+
+def certbot_dns(mh):
+        # Assume previous revoke operation succeeded and cert was deleted.
+        # We can now request a new certificate.
+
+        # Get a cert from ACME service using dns-01 challenge and Certbot's
+        # standalone HTTP server mode
+        mh.clients[0].run_command([
+            'certbot',
+            '--server', mh.acme_server,
+            'certonly',
+            '--non-interactive',
+            '--domain', mh.clients[0].hostname,
+            '--preferred-challenges', 'dns',
+            '--manual',
+            '--manual-public-ip-logging-ok',
+            '--manual-auth-hook', CERTBOT_DNS_IPA_SCRIPT,
+            '--manual-cleanup-hook', CERTBOT_DNS_IPA_SCRIPT,
+        ])
+
+def mod_md(mh):
+        # write config
+        mh.clients[0].run_command(['mkdir', '-p', '/etc/httpd/conf.d'])
+        mh.clients[0].put_file_contents(
+            '/etc/httpd/conf.d/md.conf',
+            '\n'.join([
+                f'MDCertificateAuthority {mh.acme_server}',
+                'MDCertificateAgreement accepted',
+                f'MDomain {mh.clients[0].hostname}',
+                '<VirtualHost *:443>',
+                f'    ServerName {mh.clients[0].hostname}',
+                '    SSLEngine on',
+                '</VirtualHost>\n',
+            ]),
+        )
+
+        # To check for successful cert issuance means knowing how mod_md
+        # stores certificates, or looking for specific log messages.
+        # If the thing we are inspecting changes, the test will break.
+        # So I prefer a conservative sleep.
+        #
+        mh.clients[0].run_command(['systemctl', 'restart', 'httpd'])
+        time.sleep(15)
+
+        # We expect mod_md has acquired the certificate by now.
+        # Perform a graceful restart to begin using the cert.
+        # (If mod_md ever learns to start using newly acquired
+        # certificates /without/ the second restart, then both
+        # of these sleeps can be replaced by "loop until good".)
+        #
+        mh.clients[0].run_command(['systemctl', 'reload', 'httpd'])
+        time.sleep(3)
+
+        # HTTPS request from server to client (should succeed)
+        mh.master.run_command(
+            ['curl', f'https://{mh.clients[0].hostname}'])
+
+def disable_acme_service(mh):
+        """
+        Disable ACME service again, and observe that it no longer services
+        requests.
+
+        """
+        mh.master.run_command(['ipa-acme-manage', 'disable'])
+
+        # wait a short time for Dogtag ACME service to observe config
+        # change and reconfigure itmh to no longer service requests
+        time.sleep(3)
+
+        # should fail now
+        mh.clients[0].run_command(
+            ['curl', '--fail', mh.acme_server],
+            ok_returncode=22,
+        )
 
 
 class TestACME(IntegrationTest):
@@ -87,29 +246,10 @@ class TestACME(IntegrationTest):
     #####################
 
     def test_acme_service_not_yet_enabled(self):
-        # --fail makes curl exit code 22 when response status >= 400.
-        # ACME service should return 503 because it was not enabled yet.
-        self.clients[0].run_command(
-            ['curl', '--fail', self.acme_server],
-            ok_returncode=22,
-        )
+        acme_service_not_yet_enabled(self)
 
     def test_enable_acme_service(self):
-        self.master.run_command(['ipa-acme-manage', 'enable'])
-
-        # wait a short time for Dogtag ACME service to observe config
-        # change and reconfigure itself to service requests
-        exc = None
-        for _i in range(5):
-            time.sleep(2)
-            try:
-                self.clients[0].run_command(
-                    ['curl', '--fail', self.acme_server])
-                break
-            except Exception as e:
-                exc = e
-        else:
-            raise exc
+        enable_acme_service(self)
 
     ###############
     # Certbot tests
@@ -118,56 +258,19 @@ class TestACME(IntegrationTest):
     @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
     def test_certbot_register(self):
         # service is enabled; registration should succeed
-        self.clients[0].run_command(
-            [
-                'certbot',
-                '--server', self.acme_server,
-                'register',
-                '-m', 'nobody@example.test',
-                '--agree-tos',
-                '--no-eff-email',
-            ],
-        )
+        certbot_register(self)
 
     @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
     def test_certbot_certonly_standalone(self):
         # Get a cert from ACME service using HTTP challenge and Certbot's
         # standalone HTTP server mode
-        self.clients[0].run_command(
-            [
-                'certbot',
-                '--server', self.acme_server,
-                'certonly',
-                '--domain', self.clients[0].hostname,
-                '--standalone',
-            ],
-        )
+        certbot_certonly_standalone(self)
 
     @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
     def test_certbot_revoke(self):
         # Assume previous certonly operation succeeded.
         # Read certificate to learn serial number.
-        cert_path = \
-            f'/etc/letsencrypt/live/{self.clients[0].hostname}/cert.pem'
-        data = self.clients[0].get_file_contents(cert_path)
-        cert = x509.load_pem_x509_certificate(data, backend=default_backend())
-
-        # revoke cert via ACME
-        self.clients[0].run_command(
-            [
-                'certbot',
-                '--server', self.acme_server,
-                'revoke',
-                '--cert-name', self.clients[0].hostname,
-                '--delete-after-revoke',
-            ],
-        )
-
-        # check cert is revoked (kinit already performed)
-        result = self.master.run_command(
-            ['ipa', 'cert-show', str(cert.serial_number), '--raw']
-        )
-        assert 'revocation_reason:' in result.stdout_text
+        certbot_revoke(self)
 
     @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
     def test_certbot_dns(self):
@@ -176,18 +279,7 @@ class TestACME(IntegrationTest):
 
         # Get a cert from ACME service using dns-01 challenge and Certbot's
         # standalone HTTP server mode
-        self.clients[0].run_command([
-            'certbot',
-            '--server', self.acme_server,
-            'certonly',
-            '--non-interactive',
-            '--domain', self.clients[0].hostname,
-            '--preferred-challenges', 'dns',
-            '--manual',
-            '--manual-public-ip-logging-ok',
-            '--manual-auth-hook', CERTBOT_DNS_IPA_SCRIPT,
-            '--manual-cleanup-hook', CERTBOT_DNS_IPA_SCRIPT,
-        ])
+        certbot_dns(self)
 
     ##############
     # mod_md tests
@@ -195,45 +287,7 @@ class TestACME(IntegrationTest):
 
     @pytest.mark.skipif(skip_mod_md_tests, reason='mod_md not available')
     def test_mod_md(self):
-        # write config
-        self.clients[0].run_command(['mkdir', '-p', '/etc/httpd/conf.d'])
-        self.clients[0].put_file_contents(
-            '/etc/httpd/conf.d/md.conf',
-            '\n'.join([
-                f'MDCertificateAuthority {self.acme_server}',
-                'MDCertificateAgreement accepted',
-                f'MDomain {self.clients[0].hostname}',
-                '<VirtualHost *:443>',
-                f'    ServerName {self.clients[0].hostname}',
-                '    SSLEngine on',
-                '</VirtualHost>\n',
-            ]),
-        )
-
-        # To check for successful cert issuance means knowing how mod_md
-        # stores certificates, or looking for specific log messages.
-        # If the thing we are inspecting changes, the test will break.
-        # So I prefer a conservative sleep.
-        #
-        self.clients[0].run_command(['systemctl', 'restart', 'httpd'])
-        time.sleep(15)
-
-        # We expect mod_md has acquired the certificate by now.
-        # Perform a graceful restart to begin using the cert.
-        # (If mod_md ever learns to start using newly acquired
-        # certificates /without/ the second restart, then both
-        # of these sleeps can be replaced by "loop until good".)
-        #
-        self.clients[0].run_command(['systemctl', 'reload', 'httpd'])
-        time.sleep(3)
-
-        # HTTPS request from server to client (should succeed)
-        self.master.run_command(
-            ['curl', f'https://{self.clients[0].hostname}'])
-
-    ######################
-    # Disable ACME service
-    ######################
+        mod_md(self)
 
     def test_disable_acme_service(self):
         """
@@ -241,14 +295,88 @@ class TestACME(IntegrationTest):
         requests.
 
         """
-        self.master.run_command(['ipa-acme-manage', 'disable'])
+        disable_acme_service(self)
 
-        # wait a short time for Dogtag ACME service to observe config
-        # change and reconfigure itself to no longer service requests
-        time.sleep(3)
 
-        # should fail now
-        self.clients[0].run_command(
-            ['curl', '--fail', self.acme_server],
-            ok_returncode=22,
+class TestACMEwithExternalCA(IntegrationTest):
+    """Test the FreeIPA ACME service with external CA"""
+
+    num_replicas = 0
+    num_clients = 1
+
+    @classmethod
+    def install(cls, mh):
+        # cache the acme service uri
+        acme_host = f'{IPA_CA_RECORD}.{cls.master.domain.name}'
+        cls.acme_server = f'https://{acme_host}/acme/directory'
+
+        # install packages before client install in case of IPA DNS problems
+        if not skip_certbot_tests:
+            cls.clients[0].run_command(['dnf', 'install', '-y', 'certbot'])
+        if not skip_mod_md_tests:
+            cls.clients[0].run_command(['dnf', 'install', '-y', 'mod_md'])
+
+        # install master with external-ca
+        result = install_server_external_ca_step1(cls.master)
+        assert result.returncode == 0
+        root_ca_fname, ipa_ca_fname = tasks.sign_ca_and_transport(
+            cls.master, paths.ROOT_IPA_CSR, ROOT_CA, IPA_CA
         )
+
+        install_server_external_ca_step2(
+            cls.master, ipa_ca_fname, root_ca_fname
+        )
+        tasks.kinit_admin(cls.master)
+
+        tasks.install_client(cls.master, cls.clients[0])
+        tasks.config_host_resolvconf_with_master_data(
+            cls.master, cls.clients[0]
+        )
+
+    def test_acme_service_not_yet_enabled_ext_ca(self):
+        acme_service_not_yet_enabled(self)
+
+    def test_enable_acme_service_ext_ca(self):
+        enable_acme_service(self)
+
+    @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
+    def test_certbot_register_ext_ca(self):
+        # service is enabled; registration should succeed
+        certbot_register(self)
+
+    @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
+    def test_certbot_certonly_standalone_ext_ca(self):
+        # Get a cert from ACME service using HTTP challenge and Certbot's
+        # standalone HTTP server mode
+        certbot_certonly_standalone(self)
+
+    @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
+    def test_certbot_revoke_ext_ca(self):
+        # Assume previous certonly operation succeeded.
+        # Read certificate to learn serial number.
+        certbot_revoke(self)
+
+    @pytest.mark.skipif(skip_certbot_tests, reason='certbot not available')
+    def test_certbot_dns_ext_ca(self):
+        # Assume previous revoke operation succeeded and cert was deleted.
+        # We can now request a new certificate.
+
+        # Get a cert from ACME service using dns-01 challenge and Certbot's
+        # standalone HTTP server mode
+        certbot_dns(self)
+
+    ##############
+    # mod_md tests
+    ##############
+
+    @pytest.mark.skipif(skip_mod_md_tests, reason='mod_md not available')
+    def test_mod_md_ext_ca(self):
+        mod_md(self)
+
+    def test_disable_acme_service_ext_ca(self):
+        """
+        Disable ACME service again, and observe that it no longer services
+        requests.
+
+        """
+        disable_acme_service(self)
